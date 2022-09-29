c9b299c1394548ad3fa19c6945722a0e3e71e323
Vladmspb <nevasv@yandex.ru>
2022-09-29 14:35:33 +0300

    bot admin

--- bot/models.py
+++ bot/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class botModel(models.Model):
+    # TELEGRAM_TOKEN = models.CharField(max_length=255)
+    user_id = models.IntegerField()
+    message_id = models.IntegerField()
+    # first_name
+    # last_name
+    # username
+    # is_bot
+    # sender_chat
+    # vcard
+    # phone_number
+    # longitude
+    # latitude float
+    # location location
+    # new_chat_members Array
+    text = models.CharField(max_length=255)
